7d197590bf97166965d9bd913174054a484464cf
takuya <55338+takuya@users.noreply.github.com>
2024-09-06 14:43:48 +0900

    trivial-changes

--- lib/takuya/xoauth2.rb
+++ lib/takuya/xoauth2.rb
@@ -7,6 +7,6 @@ require "googleauth/stores/file_token_store"
 require 'net/pop'
 
 ###
-require 'takuya/xoauth2/gmail-xoauth2'
-require 'takuya/xoauth2/versions'
+require_relative './xoauth2/gmail-xoauth2'
+require_relative './xoauth2/versions'
 
